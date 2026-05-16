d2dc7ab7ee0cfbe67d8689baf520debe9b786f10
Anna <anikubana@gmail.com>
2026-05-16 17:13:44 +0400

    Added lecture 2 practicing file

---
+++ Lecture 2/lecture-practicing.js
@@ -0,0 +1,16 @@
+let name = new String("Nika");
+
+let name1 = "nika";
+name1[0] = "l";
+console.log(name1);
+
+name[0] = "l";
+console.log(name);
+
+name = "Lika";
+console.log(name);
+
+let nums = [1, 2, 3, 4];
+
+let addedNum = nums.push(8);
+console.log(addedNum)
